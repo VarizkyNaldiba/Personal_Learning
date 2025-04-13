8019fbfaf998e6b5432ba7ecd9c4b4ddb6957de8
Zelectra <varizkynr@gmail.com>
2025-04-13 13:04:11 +0700

    belajar py

---
+++ Language/Phyton/another.py
@@ -0,0 +1,34 @@
+# 1
+# print("Hello Welcome to the world of Python")
+# age=20
+# price=100.50
+# name="Ky"
+# is_online=False
+# print("My age is",age)
+
+
+# 2
+# name = input("What is your name?")
+# birth_year = input("Enter your Birthyear:")
+# int(birth_year)
+# age= 2025- int(birth_year)
+
+# print("Hello",name,"Your birthyear is",birth_year,"and your age is",age)
+
+
+# 3
+first   = input("Enter first number:")
+second  = input("Enter second number:")
+int(first)
+int(second)
+
+Jumlah = int(first) + int(second)
+print("The sum of",first,"and",second,"is",Jumlah)
+Bagi = int(first) / int(second)
+print("The division of",first,"and",second,"is",Bagi)
+Kali = int(first) * int(second)
+print("The multiplication of",first,"and",second,"is",Kali)
+Kurang = int(first) - int(second)
+print("The subtraction of",first,"and",second,"is",Kurang)
+
+# 
