76f282a5d4f1d381437da01bb70603c2736f0fd3
Zelectra <varizkynr@gmail.com>
2025-04-27 14:11:43 +0700

    py py an

--- Language/Phyton/another.py
+++ Language/Phyton/another.py
@@ -17,18 +17,44 @@
 
 
 # 3
+# first   = input("Enter first number:")
+# second  = input("Enter second number:")
+# int(first)
+# int(second)
+
+# Jumlah = int(first) + int(second)
+# print("The sum of",first,"and",second,"is",Jumlah)
+# Bagi = int(first) / int(second)
+# print("The division of",first,"and",second,"is",Bagi)
+# Kali = int(first) * int(second)
+# print("The multiplication of",first,"and",second,"is",Kali)
+# Kurang = int(first) - int(second)
+# print("The subtraction of",first,"and",second,"is",Kurang)
+
+# 4
+
+
+
 first   = input("Enter first number:")
 second  = input("Enter second number:")
 int(first)
 int(second)
+def switch_case_example(option):
+    switch = {
+        1: "Jumlah",
+        2: "Kurang",
+        3: "Kali",
+        4: "Bagi",
+        5: "Keluar",
+    }
+    return switch.get(option, "Default case: Option not found")
 
-Jumlah = int(first) + int(second)
-print("The sum of",first,"and",second,"is",Jumlah)
-Bagi = int(first) / int(second)
-print("The division of",first,"and",second,"is",Bagi)
-Kali = int(first) * int(second)
-print("The multiplication of",first,"and",second,"is",Kali)
-Kurang = int(first) - int(second)
-print("The subtraction of",first,"and",second,"is",Kurang)
 
-# s
+# Jumlah = int(first) + int(second)
+# print("The sum of",first,"and",second,"is",Jumlah)
+# Bagi = int(first) / int(second)
+# print("The division of",first,"and",second,"is",Bagi)
+# Kali = int(first) * int(second)
+# print("The multiplication of",first,"and",second,"is",Kali)
+# Kurang = int(first) - int(second)
+# print("The subtraction of",first,"and",second,"is",Kurang)
